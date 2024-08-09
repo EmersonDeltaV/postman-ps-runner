296efb0aa76bf826f446f8bfe7d2217065e02826
EMRSN\E1476737 <vincentjames.paz@emerson.com>
2024-08-09 23:04:24 +0800

    feat: Add .gitignore file and collection-runner.ps1 script
    This commit adds the .gitignore file to ignore specific files in the repository, such as collection.json, environment.json, globals.json, summary.json, and JUnitReport.xml. It also includes the collection-runner.ps1 script, which is responsible for running a Postman collection using PowerShell script, generating a JUnitReport.xml, summarizing it, and sending the summary to a notificationUrl.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# postman-ps-runner
-Postman Collection Runner using powershell script
+# Introduction
+
+This PowerShell script runs a Postman collection using the specified environment and workspace's global variable, generates a JUnitReport.xml, summarizes it, saves it into a JSON file, and then sends the JSON summary file to a notification URL.
+
+# Prerequisite
+
+## Software
+
+The system that runs this PowerShell script should have the following npm packages installed globally:
+1. npm: Install it using the Node.js installer available on the cloud.
+2. newman: Install it using `npm install newman -g`.
+3. newman-reporter-junitfull: Install it using `npm install newman-reporter-junitfull -g`.
+
+## Parameters
+
+The PowerShell script requires the following parameters:
+
+1. `apiKey`: A Postman API key. Get it from your Postman account.
+2. `collectionId`: The ID of the Postman collection you would like to run.
+3. `environmentId`: The ID of the Postman environment you would like to use.
+4. `workspaceId`: The ID where the Postman collection is located. This will be used to download the global variables to be used.
+5. `systemName`: A reference to the environment being used. Readable name of the environment ID.
+6. `notificationUrl`: A POST endpoint that accepts a JSON body. This is where the JSON summary will be sent.
+
+# How to Run
+
+To run the PowerShell script, execute the following command:
+`.\collection-runner.ps1 -apiKey "PMAK-66b1b8ae71xxxxxxxx3a2277-7bexxxxxa27841e492xxxxxxxxxxb6eee5" -collectionId "32379480-dxxxxx72-1d40-4ef3-b102-xxxxxxxx3bf0" -environmentId "32xxx480-xxxxxxxx-25f2-4ca8-xxx-139xxx542f8" -workspaceId "1a63ce0e-xxxx-43a2-9251-xxxxxxx5ce9c" -systemName "PSS-120" -notificationUrl "https://www.example.com/test-result"`
---
+++ collection-runner.ps1
@@ -0,0 +1,75 @@
+﻿param (
+    [string]$apiKey,
+    [string]$collectionId,
+    [string]$environmentId,
+    [string]$workspaceId,
+    [string]$systemName,
+	[string]$notificationUrl
+)
+
+# Define the Postman API endpoints
+$collectionUrl = "https://api.getpostman.com/collections/$collectionId"
+$environmentUrl = "https://api.getpostman.com/environments/$environmentId"
+$globalVariablesUrl = "https://api.getpostman.com/workspaces/$workspaceId/global-variables?apikey=$apiKey"
+
+# Define the output file names
+$collectionFile = "collection.json"
+$environmentFile = "environment.json"
+$globalVariablesFile = "globals.json"
+
+# Download the collection
+Invoke-RestMethod -Uri $collectionUrl -Headers @{ "X-Api-Key" = $apiKey } | ConvertTo-Json -Depth 100 | Out-File $collectionFile
+
+# Download the environment
+Invoke-RestMethod -Uri $environmentUrl -Headers @{ "X-Api-Key" = $apiKey } | ConvertTo-Json -Depth 100 | Out-File $environmentFile
+
+# Download the environment
+Invoke-RestMethod -Uri $globalVariablesUrl | ConvertTo-Json -Depth 100 | Out-File $globalVariablesFile
+
+# Run Newman with the specified parameters
+newman run $collectionFile -e $environmentFile -g $globalVariablesFile -r junit --reporter-junit-export JUnitReport.xml -k
+
+# Print a success message
+Write-Host "Collection and environment downloaded successfully. Collection saved as $collectionFile, environment saved as $environmentFile."
+Write-Host "Newman run completed. JUnit report exported as JUnitReport.xml."
+
+
+# READING THE JUNITREPORT.XML
+[xml]$xml = Get-Content -Path ".\JUnitReport.xml"
+
+$totalTests = 0
+$failedTests = 0
+$failedTestList = ""
+
+# Iterate through the test cases
+foreach ($testsuite in $xml.testsuites.testsuite) {
+    $totalTests += [int]$testsuite.tests
+    foreach ($testcase in $testsuite.testcase) {
+        if ($testcase.failure) {
+            $failedTests++
+            $failedTestList += $testcase.name + "<br/>"
+        }
+    }
+}
+
+# Create a summary object
+$summary = @{
+    TotalTests = $totalTests
+    FailedTests = $failedTests
+    FailedTestList = $failedTestList
+    System = $systemName
+}
+
+# Convert the summary object to JSON and save it to a file
+$summary | ConvertTo-Json | Set-Content -Path "summary.json"
+
+
+Write-Output "Summary saved to summary.json"
+
+# SEND NOTIFICATION
+if($failedTests -ge 1) {
+	$jsonContent = Get-Content -Path ".\summary.json" -Raw
+	Invoke-RestMethod -Uri $notificationUrl -Method Post -Body $jsonContent -ContentType "application/json"
+	
+	Write-Output "Result Summary sent to notificationUrl"
+}
